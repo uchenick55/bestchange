b4f02b828cfe5f328e7b433ead6ec9782fcd8c28
uchenick55 <uchenick55@yandex.ru>
2023-03-02 23:14:50 +0300

    перевел xml в json

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Container from "react-bootstrap/Container";
 import commonClasses from "../common/CommonClasses/common.module.css";
 
-let Home = () => {
+const Home = () => {
     const home = <div>
         <Container>
             <h2 className={commonClasses.pageHeader}>Home</h2>
--- src/components/api/api.js
+++ src/components/api/api.js
@@ -10,7 +10,12 @@ const instance = axios.create({
 export let api = { // объект с методами api для USERS и follow/unfollow
     getBestChangeData: async () => {// получить данные bestChange с сервера
         const response = await instance.get()
-        return (response.data) //возврат данных из поля data
+        const convert = require("xml-js");
+        const data = JSON.parse(
+            convert.xml2json(response.data, { compact: true, spaces: 2 })
+        );
+        console.log(data.rates)
+        return (data.rates) //возврат данных из поля data
     },
 }
 
